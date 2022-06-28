af9cb98587d0e1cd2b32a6bda4ccd4e6126f8e34
IAmCellist <estuardogonzalezjr@gmail.com>
2022-06-27 19:03:03 -0500

    Basic sketch functionality and slider added

--- script.js
+++ script.js
@@ -1,23 +1,43 @@
-let gridSize = prompt("Enter the grid size: ");
+var slider = document.getElementById("myRange");
+var output = document.getElementById("demo");
+slider.oninput = function() {
+    output.innerHTML = `${this.value} x ${this.value}` ;
+}
+
+slider.addEventListener("change", () => resetGrid())
+
+let gridSize = slider.value;
 const gridContainer = document.querySelector(".grid-container");
 gridContainer.setAttribute("style", `grid-template-rows: repeat(${gridSize}, auto); 
                             grid-template-columns: repeat(${gridSize}, auto);`);
 
-for (let i = 0; i < (gridSize * gridSize); i++) {
-    let gridSquare = document.createElement("div");
-    gridSquare.addEventListener("mouseover", () => changeColor(gridSquare));
-    gridSquare.classList.add("grid-square");
-    gridContainer.append(gridSquare);
-}
+initializeGrid(gridSize);
 
-function resetGrid() {
-    let squares = document.querySelectorAll(".grid-square");
-    for (let i = 0; i < squares.length; i++) {
-        squares[i].classList.remove("color");
+function initializeGrid(gridSize) {
+    for (let i = 0; i < (gridSize * gridSize); i++) {
+        let gridSquare = document.createElement("div");
+        gridContainer.setAttribute("style", `grid-template-rows: repeat(${gridSize}, auto); 
+                            grid-template-columns: repeat(${gridSize}, auto);`);
+        gridSquare.addEventListener("mouseover", () => changeColor(gridSquare));
+        gridSquare.classList.add("grid-square");
+        gridContainer.append(gridSquare);
     }
 }
 
+function resetGrid() {
+    let squares = gridContainer.getElementsByClassName("grid-square");
+    for (let i = squares.length - 1; i >= 0; --i) {
+        squares[i].remove();
+    }
+    let newGridSize = slider.value;
+    initializeGrid(newGridSize);
+}
+
 function changeColor(gridSquare) {
-    gridSquare.classList.add("color");
+    gridSquare.setAttribute("style", "background-color: grey");
+}
+
+function removeColor(gridSquare) {
+    gridSquare.removeAttribute("background-color");
 }
 
